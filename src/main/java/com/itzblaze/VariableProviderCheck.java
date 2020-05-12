2310d764b30cb6d6acc8b8b16bd9a4fca77fd6d9
BuildTools <unconfigured@null.spigotmc.org>
2020-05-12 18:13:36 -0400

    added VariableProviderCheck

--- src/main/java/com/itzblaze/VariableProviderCheck.java
+++ src/main/java/com/itzblaze/VariableProviderCheck.java
@@ -1,4 +1,18 @@
 package com.itzblaze;
 
-public class VariableProviderCheck {
+import net.eq2online.macros.scripting.api.IVariableProvider;
+import net.eq2online.macros.scripting.parser.ScriptContext;
+import net.eq2online.macros.scripting.variable.VariableCache;
+
+public class VariableProviderCheck extends VariableCache {
+    @Override
+    public Object getVariable(String variableName) {
+        System.out.println("Start");
+        return null;
+    }
+
+    public void onInit() {
+        storeVariable("AEI",true);
+        ScriptContext.MAIN.getCore().registerVariableProvider((IVariableProvider)this);
+    }
 }
