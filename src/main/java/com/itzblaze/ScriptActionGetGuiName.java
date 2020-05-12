44300b251fd5f82a63a8d0379022e7b08d609406
BuildTools <unconfigured@null.spigotmc.org>
2020-05-12 19:27:10 -0400

    added ScriptActionGetGuiName.java

--- src/main/java/com/itzblaze/ScriptActionGetGuiName.java
+++ src/main/java/com/itzblaze/ScriptActionGetGuiName.java
@@ -1,13 +1,21 @@
 package com.itzblaze;
 
-import net.eq2online.macros.scripting.api.IMacro;
-import net.eq2online.macros.scripting.api.IMacroAction;
-import net.eq2online.macros.scripting.api.IReturnValue;
-import net.eq2online.macros.scripting.api.IScriptActionProvider;
+import net.eq2online.macros.scripting.api.*;
 import net.eq2online.macros.scripting.parser.ScriptAction;
 import net.eq2online.macros.scripting.parser.ScriptContext;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.ContainerChest;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.util.text.ITextComponent;
 
+import static net.minecraft.client.Minecraft.getMinecraft;
+
+@APIVersion(26)
 public class ScriptActionGetGuiName extends ScriptAction  {
     public ScriptActionGetGuiName() {
         super(ScriptContext.MAIN, "getguiname");
@@ -17,9 +25,14 @@ public class ScriptActionGetGuiName extends ScriptAction  {
         return false;
     }
     public IReturnValue execute(IScriptActionProvider provider, IMacro macro, IMacroAction instance, String rawParams, String[] params) {
-        IInventory inventory = null;
-        String invname = inventory.getName();
-        provider.setVariable(macro,params[0],invname);
+        EntityPlayerSP player = getMinecraft().player;
+        if (player.openContainer instanceof ContainerChest) {
+            ContainerChest chest = (ContainerChest)player.openContainer;
+            IInventory inv = chest.getLowerChestInventory();
+            String chestName = inv.getDisplayName().toString();
+            provider.setVariable(macro,params[0],chestName);
+        }
+
         return null;
     }
     @Override
